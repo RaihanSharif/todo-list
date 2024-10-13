908c763eb43505b67ce3a2ff37591c7e9bfcdffb
Raihan Sharif <raihansharif92@gmail.com>
2024-10-13 16:58:58 +0100

    Added function to return specified project from project list

--- src/projectList.js
+++ src/projectList.js
@@ -2,20 +2,24 @@ import Project from "./project.js";
 
 // a list of projects.
 class ProjectList {
-    #projects = [];
+    projects = [];
 
     addProject(project) {
-        this.#projects.push(project);
+        this.projects.push(project);
     }
 
     removeProject(project) {
-        if (this.#projects.includes(project)) {
-            const index = this.#projects.indexOf(project);
-            this.#projects.splice(index, 1);
+        if (this.projects.includes(project)) {
+            const index = this.projects.indexOf(project);
+            this.projects.splice(index, 1);
         }
     }
 
-    // TODO: compine projects?
+    // returns the matching object
+    getProject(attribute, value) {
+        return this.projects.find(proj => proj[attribute] === value);
+    }
+
     // TODO: add static function to move a task between projects
 }
 
